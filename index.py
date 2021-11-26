fd09c9bc1a01c19d870aecea3720eedf58b12571
Lucas Cunha Rodrigues <lu.cunharodrigues@hotmail.com>
2021-11-26 09:54:24 -0300

    Update index.py

--- index.py
+++ index.py
@@ -8,94 +8,57 @@ class Pessoas:
         self.opcao_de_poupanca = opcao_de_poupanca
         self.tempo_pagar = tempo_pagar
 
-    @property
-    def nome(self):
-        return self._nome
 
-    @property
-    def salario(self):
-        return self.salario
+class Produto():
 
-    @nome.setter
-    def nome(self, nome):
-        self._nome = nome
-
-    @salario.setter
-    def salario(self, salario):
-        self.salario = salario
-
-class Produto(Pessoas):
-
-    def __init__(self, nome, valor):
-        super().__init__(nome)
+    def __init__(self, nome_produto, valor):
+        self.nome_produto = nome_produto
         self.valor = valor
 
-    @property
-    def nome(self):
-        return self.nome
 
-    @nome.setter
-    def nome(self, nome):
-        self.nome = nome
-
-    @property
-    def valor(self):
-        return self.valor
-
-    @valor.setter
-    def valor(self, valor_produto):
-        self.valor = valor_produto
-
-class Poupanca(Pessoas):
+class Poupanca():
 
     def __init__(self, valor_a_guardar):
-        self._valor_a_guardar = valor_a_guardar
-
-    @property
-    def valor_a_guardar(self):
-        return self._valor_a_guardar
-
-    @valor_a_guardar.setter
-    def valor_a_guardar(self, valor_a_guardar):
-        self._valor_a_guardar = valor_a_guardar
+        self.valor_a_guardar = valor_a_guardar
 
 
-def pede_informacoes_pessoa(Pessoas):
-    Pessoas._nome = input("Qual o seu nome?")
-    Pessoas.salario = math.floor(float(input("Qual o seu salario?")))
-
-
-
-def pede_informacoes_produto(Produto):
-    Produto._nome = input("Qual o nome do produto que deseja comprar?")
-    Produto.valor = math.floor(float(input("Qual o valor do produto que deseja comprar?")))
-
-def tipo_pagamento(Pessoas, Produto, Poupanca):
+def pede_informacoes_pessoa() -> Pessoas:
+    nome = input("Qual o seu nome?")
+    salario = math.floor(float(input("Qual o seu salario?")))
     print("Você pretende guardar dinheiro como?")
-    Pessoas.opcao_de_poupanca = input("1 - Mensalmente  2 - Semanalmente  3 - Anualmente")
+    opcao_de_poupanca = input("1 - Mensalmente  2 - Semanalmente  3 - Anualmente  ")
+    tempo_pagar = math.floor(float(input("Em quantos meses quer compras o produto?")))
+    return Pessoas(nome=nome, salario=salario, opcao_de_poupanca=opcao_de_poupanca, tempo_pagar=tempo_pagar)
 
-    if(Pessoas.opcao_de_poupanca == '1'):
-        Pessoas.tempo_pagar = math.floor(float(input("Em quantos meses quer compras o produto?")))
-        Poupanca.valor_a_guardar = Produto.valor / Pessoas.tempo_pagar
-        print("O valor que precisa guardar mensalmente é: " + str("{0:.2f}".format(Poupanca.valor_a_guardar)))
 
-    elif(Pessoas.opcao_de_poupanca == '2'):
-        Pessoas.tempo_pagar = math.floor(float(input("Em quantos semanas quer compras o produto?")))
-        Poupanca.valor_a_guardar = Produto.valor / Pessoas.tempo_pagar
-        print("O valor que precisa guardar semanalmente é: " + str("{0:.2f}".format(Poupanca.valor_a_guardar)))
+def pede_informacoes_produto() -> Produto:
+    nome_produto = input("Qual o nome do produto que deseja comprar?")
+    valor = math.floor(float(input("Qual o valor do produto que deseja comprar?")))
+    return Produto(nome_produto=nome_produto, valor=valor)
+
+def tipo_pagamento(pessoa:Pessoas,produto:Produto):
+
+    if pessoa.opcao_de_poupanca == "1":
+        poupanca = Poupanca(valor_a_guardar = produto.valor / pessoa.tempo_pagar)
+        print("O valor que precisa guardar mensalmente é: " + str("{0:.2f}".format(poupanca.valor_a_guardar)))
+
+
+    elif pessoa.opcao_de_poupanca == "2":
+        poupanca = Poupanca(valor_a_guardar = produto.valor / pessoa.tempo_pagar)
+        print("O valor que precisa guardar semanalmente é: " + str("{0:.2f}".format(poupanca.valor_a_guardar)))
+
+
+    elif pessoa.opcao_de_poupanca == "3":
+        poupanca = Poupanca(valor_a_guardar = produto.valor / pessoa.tempo_pagar)
+        print("O valor que precisa guardar anualmente é: " + str("{0:.2f}".format(poupanca.valor_a_guardar)))
+
 
     else:
-        Pessoas.tempo_pagar = math.floor(float(input("Em quantos anos quer compras o produto?")))
-        Poupanca.valor_a_guardar = Produto.valor / Pessoas.tempo_pagar
-        print("O valor que precisa guardar anualmente é: " + str("{0:.2f}".format(Poupanca.valor_a_guardar)))
+        print("Está não é opção válida!")
 
-
-
-
-
-pede_informacoes_pessoa(Pessoas)
-pede_informacoes_produto(Produto)
-tipo_pagamento(Pessoas, Produto, Poupanca)
+pessoa1 = pede_informacoes_pessoa()
+produto1 = pede_informacoes_produto()
+tipo_pagamento(pessoa=pessoa1, produto=produto1)
 
 
 
